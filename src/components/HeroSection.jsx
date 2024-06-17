7dcbe1941b4e350838124d13e31dd2944aa676e4
Callistus Anwara <callistusanwara@gmail.com>
2024-06-17 06:34:41 +0100

    hero icon updated

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
         Finding the right fit
         <img
           id={`${styles.heroIcon}`}
-          src="https://s3-alpha-sig.figma.com/img/77ea/e46e/4921166cf2646c0f6f42c6cf4855a5ea?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VQ7VkPEEUxuVcL92P4UX0qcv7PZVjpY8FtvTB6wBMltLDzmOdRegfRvDT9qeUyFRsIn7cbjzaq2i0tse6f4RAQNOAMXsFUjU~ZtMrsHSHPesHP7IuWnCxpTskvub9-zdkz1VHzTa6Iyvvq54K3-CqVJdylBHMEJ2Kyx--VniitefF8viL7Wy0qiHPzQ-Uiu89vmBO3YgI6050R6ZRA33Z9G3VyWN1V2LnyZzPD17wAQJ0Lnxje27cQoQOS45TcMMCIL3wdBycw1lecH-j2v04zrEjOAPcyzh27cOIo7rXnm744lBdY9CHrynQOwdy4z4Dex1CCMQKMS07SOcTHvbuA__"
+          src="https://s3-alpha-sig.figma.com/img/77ea/e46e/4921166cf2646c0f6f42c6cf4855a5ea?Expires=1719792000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=letDW-HpVLmzTxWncnTX6dDVPxxge1W7~YNtoGwkIIp-2d~1vqnd0TaH~aYccroZN60WAlr8tGOHL5fT~yeGoh25Euriv-QM26LjdVR6DDJGfTmHiXLbPdSd7oGgnJ6Ck6DdNKvfghxsBSqeiN0aEwG5hOOowL8Wf7trBm33xbYPkDV31d53Czi4nW~UePc95otLtqs2zNDLJyiUSyccKdT4pN6wHWE8gUzAGJXVsDVklg1wB-LgvFJyq2qCA0c3VhwA4bin6TsEWk7nQzzwTzJqZfKzHqBlq5LfRsNwaq2SnsX683J0-YlQaGr8nbQbv64vYjic2TNTRiwIyB1LEw__"
           alt="heroimage"
         />
         has never been easier.
